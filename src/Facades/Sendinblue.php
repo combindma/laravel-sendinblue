a7824f95e8526de652b525b5fef4b08bff23322e
Combind <webmaster@combind.ma>
2023-03-22 15:43:13 +0000

    wip

--- src/Facades/Sendinblue.php
+++ src/Facades/Sendinblue.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Facade;
  */
 class Sendinblue extends Facade
 {
-    protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor(): string
     {
         return 'laravel-sendinblue';
     }
